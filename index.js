b0a053b35cc68f305f6ece09d96cdd56827b7e3b
QuocDung <rongvang2357hn@gmail.com>
2022-08-16 10:27:32 +0700

    update

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  var a = 1;
+  var b = 2;
+  var c = a + b;
+  res.send("Hello World !");
 });
-
+app.get("/home", (req, res) => {
+  res.send("Hello Home !");
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
