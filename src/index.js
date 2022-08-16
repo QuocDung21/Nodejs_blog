a85ee45de20fda4b1c55c7e17bd9637d873f3551
QuocDung <rongvang2357hn@gmail.com>
2022-08-16 11:50:52 +0700

    update

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const morgan = require("morgan");
+const { engine } = require("express-handlebars");
+const port = 3001;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(morgan("combined"));
+// Template engine
+
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+console.log("Paht :>> ", path.join(__dirname, "resources/views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/news", (req, res) => {
+  res.render("news");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
